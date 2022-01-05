c953b7357d0324a4a324e4cdb78b18e2641f5bec
LNGupta <campustray@gmail.com>
2022-01-05 21:23:32 +0530

    model added and migration done

--- crib_asgmt/supermarket/models.py
+++ crib_asgmt/supermarket/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.IntegerField(default=0)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
